af179db6ce21b94b4f9389afedb1b1d95a2b0eca
Katrin Sigurdardottir <katrinduasig@gmail.com>
2018-05-18 22:32:16 +0200

    sidemenu all

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -6,9 +6,9 @@ class Sidebar extends Component {
     return (
 
       <nav className="sidebar-container">
-        <Link to="/" className="skjult-menu">Skjult menu<img src={require('./closeMenu.png')} alt=""/></Link>
+        <Link to="/" className="skjult-menu">Skjult menu<img src={require('./images/closeMenu.png')} alt=""/></Link>
         <div className="user-info">
-          <img src={require('./user.png')} alt="user picture"/>
+          <img src={require('./images/user.png')} alt="user picture"/>
           <h2>Katrín Sigurðardóttir</h2>
           <div className="follow">
             <div className="follow-item">
@@ -24,33 +24,33 @@ class Sidebar extends Component {
         <div className="section">
           <h3>Library</h3>
           <ul className="section-list">
-            <li><Link to="/kigger-pa-nu"><img src={require('./heart.svg')} alt=""/> Kigger pa nu</Link></li>
-            <li><Link to="/for-nylig-set"><img src={require('./heart.svg')} alt=""/> For nylig set</Link></li>
-            <li><Link to="/anbefalet"><img src={require('./heart.svg')} alt=""/> Anbefalet</Link></li>
-            <li className="sectionActive"><Link to="/nye-trailere"><img src={require('./heartYellow.svg')} alt=""/> Nye trailere</Link></li>
-            <li><Link to="/kommer-snart"><img src={require('./heart.svg')} alt=""/> Kommer snart</Link></li>
+            <li><Link to="/kigger-pa-nu"><img src={require('./images/heart.svg')} alt=""/> Kigger pa nu</Link></li>
+            <li><Link to="/for-nylig-set"><img src={require('./images/heart.svg')} alt=""/> For nylig set</Link></li>
+            <li><Link to="/anbefalet"><img src={require('./images/heart.svg')} alt=""/> Anbefalet</Link></li>
+            <li className="sectionActive"><Link to="/nye-trailere"><img src={require('./images/heartYellow.svg')} alt=""/> Nye trailere</Link></li>
+            <li><Link to="/kommer-snart"><img src={require('./images/heart.svg')} alt=""/> Kommer snart</Link></li>
           </ul>
         </div>
         <div className="section">
           <h3>Collections</h3>
           <ul className="section-list">
-            <li><Link to="/kigger-pa-nu"><img src={require('./star.svg')} alt=""/> Favorit</Link></li>
-            <li><Link to="/for-nylig-set"><img src={require('./star.svg')} alt=""/> Olden goldies</Link></li>
-            <li><Link to="/anbefalet"><img src={require('./star.svg')} alt=""/> Chick Flicks</Link></li>
-            <li><Link to="/nye-trailere"><img src={require('./star.svg')} alt=""/>Splatter films</Link></li>
-            <li><Link to="/kommer-snart"><img src={require('./star.svg')} alt=""/> Anjas beste</Link></li>
-            <li><Link to="/kommer-snart"><img src={require('./star.svg')} alt=""/> Lars Von Trier</Link></li>
+            <li><Link to="/kigger-pa-nu"><img src={require('./images/star.svg')} alt=""/> Favorit</Link></li>
+            <li><Link to="/for-nylig-set"><img src={require('./images/star.svg')} alt=""/> Olden goldies</Link></li>
+            <li><Link to="/anbefalet"><img src={require('./images/star.svg')} alt=""/> Chick Flicks</Link></li>
+            <li><Link to="/nye-trailere"><img src={require('./images/star.svg')} alt=""/>Splatter films</Link></li>
+            <li><Link to="/kommer-snart"><img src={require('./images/star.svg')} alt=""/> Anjas beste</Link></li>
+            <li><Link to="/kommer-snart"><img src={require('./images/star.svg')} alt=""/> Lars Von Trier</Link></li>
           </ul>
         </div>
         <div className="ny">
-        <img src={require('./add.png')} alt=""/>
+        <img src={require('./images/add.png')} alt=""/>
         <p>Ny collection</p>
         </div>
         <div className="section">
           <h3>Your Account</h3>
           <ul className="section-list">
-            <li><Link to="/kigger-pa-nu"><img src={require('./profile.svg')} alt=""/> Min profile</Link></li>
-            <li><Link to="/for-nylig-set"><img src={require('./settings.svg')} alt=""/> Settings</Link></li>
+            <li><Link to="/kigger-pa-nu"><img src={require('./images/profile.svg')} alt=""/> Min profile</Link></li>
+            <li><Link to="/for-nylig-set"><img src={require('./images/settings.svg')} alt=""/> Settings</Link></li>
           </ul>
         </div>
       </nav>
